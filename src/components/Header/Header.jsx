8a42ef840800f5e5c6e7e0e39602053ae71f5734
Emirlan Dogdurbaev <jigit38@gmail.com>
2024-10-10 14:08:35 +0600

    added about us section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,19 @@
+import classes from "./Header.module.scss"
+
 const Header = () => {
-    return(
-        <header className="">
-            header
+    return (
+        <header className={classes.header}>
+            <div className={classes.inner_cont}>
+                <div>
+                    <h2>About us</h2>
+                    <span className={classes.small_line}></span>
+                    <p>
+                        We serve amazing dishes with authentic and signature taste! We serve pasta, pizza, salads, prime
+                        cut
+                        steaks and more... we also have our coffee shop which we serve one of the best roasted coffee.
+                    </p>
+                </div>
+            </div>
         </header>
     )
 }
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,10 +1,15 @@
+import classes from './Nav.module.scss';
 import logo from '../../assets/images/logo.png';
 
 const Nav = () => {
     return (
-        <nav className="nav">
-            <img src={logo} alt=""/>
-        </nav>
+        <>
+            <nav className={classes.nav}>
+                <img src={logo} alt="logotype" height={170}/>
+            </nav>
+            <div className={classes.line}>
+            </div>
+        </>
     )
 }
 
