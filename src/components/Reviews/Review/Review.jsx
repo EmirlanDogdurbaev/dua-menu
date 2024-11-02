d815b7bf9b74f930858db00e6463f8bdb96e7ccb
Emirlan Dogdurbaev <jigit38@gmail.com>
2024-11-02 22:10:00 +0600

    test adaptive for 2560px

--- src/components/Reviews/Review/Review.jsx
+++ src/components/Reviews/Review/Review.jsx
@@ -4,7 +4,6 @@ import classes from "./Review.module.scss";
 const Review = ({data}) => {
     return (<div className={classes.Review}>
         <img src={data.picture} alt=""/>
-        {/*<h1>{data.title}</h1>*/}
     </div>)
 }
 
